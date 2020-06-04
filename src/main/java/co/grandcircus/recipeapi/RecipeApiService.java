ec8f344890f18db83cbf4222ffc322cd1df3bb02
Tom Rowland <thomasjrowland@gmail.com>
2020-06-04 13:53:53 -0400

    finished api service

--- src/main/java/co/grandcircus/recipeapi/RecipeApiService.java
+++ src/main/java/co/grandcircus/recipeapi/RecipeApiService.java
@@ -1,13 +1,33 @@
 package co.grandcircus.recipeapi;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import co.grandcircus.recipeapi.model.Recipe;
+import co.grandcircus.recipeapi.model.RecipeApiResponse;
 
 @Service
 public class RecipeApiService {
 	
 	private RestTemplate rest = new RestTemplate();
 	
+	@Value("${ app.id }")
+	String appId; 
 	
+	@Value("${ app.key }")
+	String appKey;
+	
+	public RecipeApiResponse recipeSearch(String searchTerms) {
+		
+		String url = "https://api.edamam.com/search?q=" + searchTerms + "&app_id=" + appId + "&app_key=" + appKey;
+		
+		RecipeApiResponse searchResult = rest.getForObject(url, RecipeApiResponse.class);
+		
+		return searchResult;
+		
+	}
 
 }
