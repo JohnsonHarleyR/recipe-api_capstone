ba161a8218cfea94448b3898f8becd131ce17901
harley <harleymakesart@gmail.com>
2020-06-04 15:52:43 -0400

    Added User class

---
+++ src/main/java/co/grandcircus/recipeapi/model/User.java
@@ -0,0 +1,73 @@
+package co.grandcircus.recipeapi.model;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String email;
+	private String password;
+	private String name;
+	
+	public User() {
+		
+	}
+	
+	public User(String username, String email, String password, String name) {
+		this.username = username.trim();
+		this.email = email.trim();
+		this.password = password;
+		this.name = name.trim(); //I thought about the trim for when the name gets displayed
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email +
+				", password=" + password + ", name=" + name + "]";
+	}
+	
+
+}
